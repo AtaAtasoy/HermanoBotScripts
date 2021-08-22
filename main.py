5dee28d472400762880991f2d2d51cc94815957b
Ata Atasoy <h.ataatasoy@gmail.com>
2021-08-22 16:32:40 +0300

    port fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-if __name__ == "main":
+if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
