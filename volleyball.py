d3b737a4da11a690a543a8015d4e48b6b6415404
Ata Atasoy <h.ataatasoy@gmail.com>
2021-11-18 21:54:03 +0300

    fix event url, fix title and location strings

--- volleyball.py
+++ volleyball.py
@@ -11,23 +11,24 @@ volleyball_module = Blueprint('volleyball', __name__)
 @volleyball_module.route('/')
 def volleyball():
     try:
-        url = os.getenv('VOLLEYBALL_TICKET_URL')
-        webpage = requests.get(url)
+        ticket_seller_url = os.getenv('VOLLEYBALL_TICKET_SELLER_URL')
+        volleyball_games_url = os.getenv('VOLLEYBALL_GAMES_URL')
+        webpage = requests.get(volleyball_games_url)
         soup = BeautifulSoup(webpage.content, 'html.parser')
         results = soup.find("div", class_="resultsGrid")
         children = results.findAll("a", recursive=False)
         schedule = []
 
         for child in children:
-            title = child.span.text
-            location = child.p.text
+            title = ''.join([i for i in child.span.text if i.isalnum() or i == "-" or i == "(" or i == ")"])
+            location = ''.join([i for i in child.span.text if i.isalnum() or i == "-" or i == "(" or i == ")"])
             date = ''.join([i for i in child.find("p", class_="dates").text if i.isalnum() or i == "-"])
             
             result = {
                 "title": title,
                 "location": location,
                 "date": date,
-                "ticket_url": url + child["href"]
+                "ticket_url": ticket_seller_url + child["href"]
             }
 
             schedule.append(result)            
