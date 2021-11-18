a3d639de3e78335d855c51b63eae17932ad9d270
Ata Atasoy <h.ataatasoy@gmail.com>
2021-11-18 22:30:07 +0300

    switch string encoding for json

--- volleyball.py
+++ volleyball.py
@@ -1,7 +1,5 @@
-from os import abort
 from bs4 import BeautifulSoup
-from flask import Blueprint
-from flask.json import jsonify
+from flask import Blueprint, json
 from werkzeug.exceptions import NotFound
 import requests
 import os
@@ -12,16 +10,16 @@ volleyball_module = Blueprint('volleyball', __name__)
 def volleyball():
     try:
         ticket_seller_url = os.getenv('VOLLEYBALL_TICKET_SELLER_URL')
-        volleyball_games_url = os.getenv('VOLLEYBALL_GAMES_URL')
-        webpage = requests.get(volleyball_games_url)
+        volleyball_matches_url = os.getenv('VOLLEYBALL_MATCHES_URL')
+        webpage = requests.get(volleyball_matches_url)
         soup = BeautifulSoup(webpage.content, 'html.parser')
         results = soup.find("div", class_="resultsGrid")
         children = results.findAll("a", recursive=False)
         schedule = []
 
         for child in children:
-            title = ''.join([i for i in child.span.text if i.isalnum() or i == "-" or i == "(" or i == ")"])
-            location = ''.join([i for i in child.span.text if i.isalnum() or i == "-" or i == "(" or i == ")"])
+            title = child.span.text
+            location = child.p.text
             date = ''.join([i for i in child.find("p", class_="dates").text if i.isalnum() or i == "-"])
             
             result = {
@@ -31,9 +29,10 @@ def volleyball():
                 "ticket_url": ticket_seller_url + child["href"]
             }
 
-            schedule.append(result)            
-        return jsonify(schedule, 200)
+            schedule.append(result)
 
-    except NotFound:
-        print("Not Found")
-        abort(404)
+        response_data = json.dumps(schedule, ensure_ascii=False).encode("utf8")
+        return response_data
+
+    except Exception:
+        return {"message": "Could not fetch ticket information", "status": "500"}
