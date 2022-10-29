56e91c77d1f9608fc0c6030301b4861f3b03cdba
Ata Atasoy <h.ataatasoy@gmail.com>
2022-10-30 01:24:19 +0300

    add expansions for the twit-search results

--- twitter.py
+++ twitter.py
@@ -10,16 +10,25 @@ twitter_module = Blueprint('twitter', __name__)
 load_dotenv()
 
 @twitter_module.route('/')
-def performTwitterSearch(): 
+def performTwitterSearch():
     query_string = request.args.get('query')
     if not query_string:
-        return json.dump({"message": "did not receive parameter 'query'", "status": HTTPStatus.BAD_REQUEST})
+        return json.dump({
+            "message": "did not receive parameter 'query'",
+            "status": HTTPStatus.BAD_REQUEST
+        })
 
-    max_results = request.args.get('max_results') if request.args.get('max_results') is not None  else 10
-    sort_order = request.args.get('sort_order') if request.args.get('sort_order') is not None  else 'recency'
+    max_results = request.args.get('maxResults') if request.args.get(
+        'maxResults') is not None else 10
+    sort_order = request.args.get('sortOrder') if request.args.get(
+        'sortOrder') is not None else 'recency'
 
-    client = tweepy.Client(bearer_token=  os.getenv('BEARER_TOKEN'))
-    tweets = client.search_recent_tweets(query=query_string, max_results=max_results, sort_order=sort_order)
+    client = tweepy.Client(bearer_token=os.getenv('BEARER_TOKEN'))
+    tweets = client.search_recent_tweets(
+        query=query_string,
+        max_results=max_results,
+        sort_order=sort_order,
+        expansions=['author_id', 'referenced_tweets.id'])
 
     response = jsonpickle.encode(tweets.data)
 
